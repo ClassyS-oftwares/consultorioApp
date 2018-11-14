dd1ceee07064b2b4f2efb5162f749fea15d11a98
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-14 11:23:54 -0200

    Fixed the booleans, create the missing tables and aded all the foreign keys remaining

--- clinica.sql
+++ clinica.sql
@@ -18,13 +18,19 @@ crm int not null unique,
 especialidade varchar(50) not null
 )engine=innodb;
 
+#Criando chave estrangeira para doutor herdar colunas de pessoa
+alter table doutor add constraint fk_pessoa_doutor foreign key (id_pessoa) references pessoa(id_pessoa);
+
 create table paciente(
 id_paciente int not null primary key auto_increment,
 id_pessoa int not null,
-#presenca boolean
+presenca tinyint unsigned not null default 0
 )engine=innodb;
 
-alter table doutor add constraint fk_pessoa_doutor foreign key (id_pessoa) references pessoa(id_pessoa);
+#Reaproveitando uma coluna para uma outra função
+alter table paciente change presenca pendencia tinyint unsigned not null default 0;
+#Criando chaves estrangeiras para paciente herdar colunas de pessoa
+alter table paciente add constraint fk_pessoa_paciente foreign key (id_pessoa) references pessoa(id_pessoa);
 
 create table endereco(
 id_endereco int not null primary key auto_increment,
@@ -37,6 +43,7 @@ cidade varchar (50) not null,
 estado varchar (50) not null
 );
 
+#Criando chaves estrangeiras para relacionar uma pesso e seu endereço
 alter table endereco add constraint fk_pessoa_endereco foreign key (id_pessoa) references pessoa(id_pessoa);
 
 create table consulta(
@@ -47,21 +54,31 @@ data_consulta date not null,
 horario_consulta time not null
 )engine=innodb;
 
+#Criando chaves estrangeiras de consulta
+alter table consulta add constraint fk_paciente_consulta foreign key (id_paciente) references pessoa(id_pessoa);
+alter table consulta add constraint fk_doutor_consulta foreign key (id_doutor) references doutor(id_doutor);
+
 create table Pagamento(
 id_pagamento int not null primary key auto_increment,
-#status_pgto boolean
+status_pgto tinyint unsigned not null default 0,
 valor_pgto decimal (5,2) not null default 000.00,
 data_pgto date not null,
 forma enum('Dinheiro','Debito') not null default 'Dinheiro'
 )engine=innodb;
+
+#dicionando chave estrangeira
+alter table pagamento add column id_consulta int not null;
+alter table pagamento add constraint  fk_consulta_pagamento foreign key (id_consulta) references consulta(id_consulta);
 
 create table agenda(
 id_agenda int not null primary key auto_increment,
 id_doutor int not null,
 dia date not null,
 horario time not null,
-#cancelamento boolean
+cancelamento tinyint unsigned not null default 0
 )engine=innodb;
+
+alter table agenda add constraint fk_doutor_agenda foreign key (id_doutor) references doutor(id_doutor);
 
 create table conta_login(
 id_conta int not null primary key auto_increment,
